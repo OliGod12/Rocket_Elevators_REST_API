7d41555c0677f1d3e3fd889c2236481b863fe523
samishoux <samuel.chabot@live.ca>
2020-08-03 18:05:20 -0400

    first commit

---
+++ BlogPost.cs
@@ -0,0 +1,76 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace Rocket_Elevators_REST_API
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        internal AppDb Db { get; set; }
+
+        public BlogPost()
+        {
+        }
+
+        internal BlogPost(AppDb db)
+        {
+            Db = db;
+        }
+
+        public async Task InsertAsync()
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"INSERT INTO `rocketApp_development` (`Title`, `Content`) VALUES (@title, @content);";
+            BindParams(cmd);
+            await cmd.ExecuteNonQueryAsync();
+            Id = (int)cmd.LastInsertedId;
+        }
+
+        public async Task UpdateAsync()
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"UPDATE `rocketApp_development` SET `Title` = @title, `Content` = @content WHERE `Id` = @id;";
+            BindParams(cmd);
+            BindId(cmd);
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        public async Task DeleteAsync()
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"DELETE FROM `rocketApp_development` WHERE `Id` = @id;";
+            BindId(cmd);
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        private void BindId(MySqlCommand cmd)
+        {
+            cmd.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = "@id",
+                DbType = DbType.Int32,
+                Value = Id,
+            });
+        }
+
+        private void BindParams(MySqlCommand cmd)
+        {
+            cmd.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = "@title",
+                DbType = DbType.String,
+                Value = Title,
+            });
+            cmd.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = "@content",
+                DbType = DbType.String,
+                Value = Content,
+            });
+        }
+    }
+}
