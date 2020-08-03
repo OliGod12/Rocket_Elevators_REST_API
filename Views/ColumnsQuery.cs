36185d325e040f68db35cf2aefad7deec10b2044
samishoux <samuel.chabot@live.ca>
2020-08-03 19:12:44 -0400

    better first commit

--- Views/ColumnsQuery.cs
+++ Views/ColumnsQuery.cs
@@ -3,22 +3,23 @@ using System.Data;
 using System.Data.Common;
 using System.Threading.Tasks;
 using MySqlConnector;
+using Rocket_Elevators_REST_API.Models;
 
-namespace Rocket_Elevators_REST_API
+namespace Rocket_Elevators_REST_API.Views
 {
-    public class BlogPostQuery
+    public class ColumnsQuery
     {
         public AppDb Db { get; }
 
-        public BlogPostQuery(AppDb db)
+        public ColumnsQuery(AppDb db)
         {
             Db = db;
         }
 
-        public async Task<BlogPost> FindOneAsync(int id)
+        public async Task<Columns> FindOneAsync(int id)
         {
             using var cmd = Db.Connection.CreateCommand();
-            cmd.CommandText = @"SELECT `Id`, `status` FROM `batteries` WHERE `Id` = @id";
+            cmd.CommandText = @"SELECT `Id`, `status` FROM `columns` WHERE `Id` = @id";
             cmd.Parameters.Add(new MySqlParameter
             {
                 ParameterName = "@id",
@@ -29,34 +30,33 @@ namespace Rocket_Elevators_REST_API
             return result.Count > 0 ? result[0] : null;
         }
 
-        public async Task<List<BlogPost>> LatestPostsAsync()
+        public async Task<List<Columns>> LatestPostsAsync()
         {
             using var cmd = Db.Connection.CreateCommand();
-            cmd.CommandText = @"SELECT `Id`, `status` FROM `batteries` ORDER BY `Id` DESC LIMIT 10;";
+            cmd.CommandText = @"SELECT `Id`, `status` FROM `columns` ORDER BY `Id` DESC LIMIT 10;";
             return await ReadAllAsync(await cmd.ExecuteReaderAsync());
         }
 
-        public async Task DeleteAllAsync()
-        {
-            using var txn = await Db.Connection.BeginTransactionAsync();
-            using var cmd = Db.Connection.CreateCommand();
-            cmd.CommandText = @"DELETE FROM `rocketApp_development`";
-            await cmd.ExecuteNonQueryAsync();
-            await txn.CommitAsync();
-        }
+        //public async Task DeleteAllAsync()
+        //{
+        //    using var txn = await Db.Connection.BeginTransactionAsync();
+        //    using var cmd = Db.Connection.CreateCommand();
+        //    cmd.CommandText = @"DELETE FROM `rocketApp_development`";
+        //    await cmd.ExecuteNonQueryAsync();
+        //    await txn.CommitAsync();
+        //}
 
-        private async Task<List<BlogPost>> ReadAllAsync(DbDataReader reader)
+        private async Task<List<Columns>> ReadAllAsync(DbDataReader reader)
         {
-            var posts = new List<BlogPost>();
+            var posts = new List<Columns>();
             using (reader)
             {
                 while (await reader.ReadAsync())
                 {
-                    var post = new BlogPost(Db)
+                    var post = new Columns(Db)
                     {
                         Id = reader.GetInt32(0),
-                        Title = reader.GetString(1),
-                        Content = reader.GetString(2),
+                        Status = reader.GetString(1),
                     };
                     posts.Add(post);
                 }
