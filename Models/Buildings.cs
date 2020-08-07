6ba359b8dc7d5ae49dada0f7f8f972bc2f634e44
samishoux <samuel.chabot@live.ca>
2020-08-07 17:07:34 -0400

    update time when puts

--- Models/Buildings.cs
+++ Models/Buildings.cs
@@ -44,7 +44,7 @@ namespace Rocket_Elevators_REST_API.Models
         public async Task UpdateAsync()
         {
             using var cmd = Db.Connection.CreateCommand();
-            cmd.CommandText = @"UPDATE `buildings` SET `status` = @status WHERE `Id` = @id;";
+            cmd.CommandText = @"UPDATE `buildings` SET `status` = @status, `updated_at` = NOW() WHERE `Id` = @id;";
             //BindParams(cmd);
             BindId(cmd);
             await cmd.ExecuteNonQueryAsync();
